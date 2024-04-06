994df623e26422841616106d431d2c8bee368518
David García Díaz <davidgrcdiaz@gmail.com>
2024-04-06 17:02:17 +0200

    Add database types

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,7 +1,8 @@
 
+import type { Database } from "@/types/database";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseKey = import.meta.env.SUPABASE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
